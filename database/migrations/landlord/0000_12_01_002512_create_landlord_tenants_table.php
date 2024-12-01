52287e5f119c83675796da782605a3f7a009c2e8
Lawrence Njoroge <lawlens1@gmail.com>
2024-12-01 03:26:58 +0300

    Refactor migrations to create landlord-specific tables for tenants, users, cache, and jobs

--- database/migrations/landlord/0000_12_01_002512_create_landlord_tenants_table.php
+++ database/migrations/landlord/0000_12_01_002512_create_landlord_tenants_table.php
@@ -9,19 +9,13 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('tenants', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('database')->unique();
             $table->timestamps();
         });
-    }
-
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('tenants');
     }
 };
